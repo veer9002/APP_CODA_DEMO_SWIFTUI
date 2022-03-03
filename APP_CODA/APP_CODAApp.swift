0b007a0d5a387d3b5e9faddb60a451a2ca51fbbc
Manish Sharma <manish.sharma@cimcondigital.com>
2022-03-03 15:34:48 +0530

    Initial Commit

---
+++ APP_CODA/APP_CODAApp.swift
@@ -0,0 +1,17 @@
+//
+//  APP_CODAApp.swift
+//  APP_CODA
+//
+//  Created by Manish  Sharma on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct APP_CODAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APP_CODA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APP_CODA
+//
+//  Created by Manish  Sharma on 03/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
